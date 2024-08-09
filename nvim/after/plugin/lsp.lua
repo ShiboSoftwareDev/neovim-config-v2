05eeedb14032524db3b1034ff8c4e93644f9af35
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2024-08-09 14:22:13 +0000

    updated lsp and cmp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,42 +1,59 @@
-local lsp_zero = require('lsp-zero')
+local lsp = require('lsp-zero')
 
-local lsp_attach = function(client, bufnr)
-    local opts = {buffer = bufnr}
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-    end
-    lsp_zero.extend_lspconfig({
-        sign_text = true,
-        lsp_attach = lsp_attach,
-        capabilities = require('cmp_nvim_lsp').default_capabilities(),
-    })
+local lsp_attach = function(_, bufnr)
+    local opts = { buffer = bufnr }
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+end
 
-    local cmp = require('cmp')
+lsp.extend_lspconfig({
+    sign_text = true,
+    lsp_attach = lsp_attach,
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
+
+lsp.ui({
+    float_border = 'rounded',
+    sign_text = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I',
+    },
+})
+
+local cmp = require('cmp')
+
+local cmp_format = require('lsp-zero').cmp_format({ details = true })
+
+cmp.setup({
+
+    sources = {
+
+        { name = 'nvim_lsp' },
+
+    },
+
+    snippet = {
+
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
 
 
+    },
 
-    cmp.setup({
+    formatting = cmp_format,
 
-          sources = {
-
-                  {name = 'nvim_lsp'},
-
-                    },
-
-                      snippet = {
-
-                              expand = function(args)
-
-                                               vim.snippet.expand(args.body)
-                                                   end,
-                                                     },
-                                                       mapping = cmp.mapping.preset.insert({}),
-                                                    })
+    mapping = cmp.mapping.preset.insert({
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    }),
+})
